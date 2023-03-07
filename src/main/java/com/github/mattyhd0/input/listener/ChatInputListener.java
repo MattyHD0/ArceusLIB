0c1eee89ef7b7004dfa9b44cf5454f7f0ba215d2
MattyHD0 <MattyHD0@hotmail.com>
2023-03-06 23:17:41 -0300

    Changed AsyncChatListener to CommandListener to prevent CompletableFuture incompatibilities

--- src/main/java/com/github/mattyhd0/input/listener/ChatInputListener.java
+++ src/main/java/com/github/mattyhd0/input/listener/ChatInputListener.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 import java.util.UUID;
 
@@ -21,7 +22,7 @@ public class ChatInputListener implements Listener {
     }
 
     @EventHandler
-    public void onPlayerChat(AsyncPlayerChatEvent event){
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){
 
         Player player = event.getPlayer();
         UUID playerUuid = player.getUniqueId();
