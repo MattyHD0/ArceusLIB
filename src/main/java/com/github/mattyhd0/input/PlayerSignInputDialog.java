cb8637a98d4276197ee287f11fe75845594329b3
MattyHD0 <MattyHD0@hotmail.com>
2023-08-27 22:06:41 -0300

    improved: PlayerSignInputDialog#send

--- src/main/java/com/github/mattyhd0/input/PlayerSignInputDialog.java
+++ src/main/java/com/github/mattyhd0/input/PlayerSignInputDialog.java
@@ -1,6 +1,7 @@
 package com.github.mattyhd0.input;
 
 import com.github.mattyhd0.input.action.InputProcessor;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -37,8 +38,9 @@ public class PlayerSignInputDialog extends AbstractPlayerInputDialog {
         Block block = location.getBlock();
         BlockData blockData = block.getBlockData().clone();
 
-        block.setType(Material.OAK_SIGN);
-        Sign sign = (Sign) block.getState();
+        Sign sign = (Sign) Bukkit.createBlockData(Material.OAK_SIGN);
+
+        block.setBlockData(sign.getBlockData(), false);
 
         for (int i = 0; i < 4 || i < lines.length; i++){
             sign.setLine(i, lines[i]);
@@ -50,7 +52,7 @@ public class PlayerSignInputDialog extends AbstractPlayerInputDialog {
         }*/
 
         player.openSign(sign);
-        block.setBlockData(blockData);
+        block.setBlockData(blockData, false);
 
     }
 
