34f975c8e050e7efd6f3b1ea492c2fa7f5537c23
MattyHD0 <MattyHD0@hotmail.com>
2023-09-02 12:22:49 -0300

    fix: changed not scheduled executor to scheduled

--- src/main/java/com/github/mattyhd0/input/PlayerChatInputDialog.java
+++ src/main/java/com/github/mattyhd0/input/PlayerChatInputDialog.java
@@ -32,7 +32,7 @@ public class PlayerChatInputDialog extends AbstractPlayerInputDialog {
     @Override
     public void send(Player player) {
 
-        executorService.schedule(() -> {
+        executorService.scheduleAtFixedRate(() -> {
 
             if(!player.isOnline() || player.isDead()) return;
 
@@ -49,7 +49,7 @@ public class PlayerChatInputDialog extends AbstractPlayerInputDialog {
                 );
             }
 
-        }, 1000, TimeUnit.MILLISECONDS);
+        },0,100, TimeUnit.MILLISECONDS);
     }
 
     @Override
